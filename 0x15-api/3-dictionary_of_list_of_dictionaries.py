cc8b54a9a2745aa5c43906cf5be91f27f8efe23a
Nic Basilio <bnbasilio@gmail.com>
2020-09-15 06:58:09 +0000

    task 3 completeD

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" 3. Dictionary of list of dictionaries """
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+users = requests.get('https://jsonplaceholder.typicode.com/users')
+output = {}
+for user in users.json():
+    data = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(user.get('id')))
+    tasks = [{"task": _.get('title'), "completed": _.get('completed'),
+              "username": user.get('username')} for _ in data.json()]
+    output[user.get('id')] = tasks
+
+with open('todo_all_employees.json', 'w') as outfile:
+    json.dump(output, outfile)
