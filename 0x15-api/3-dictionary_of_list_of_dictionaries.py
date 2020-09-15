b260aaa04bdda2c36792d803807db62f98d4853d
Nic Basilio <bnbasilio@gmail.com>
2020-09-15 06:59:36 +0000

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,15 +6,15 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-users = requests.get('https://jsonplaceholder.typicode.com/users')
-output = {}
-for user in users.json():
-    data = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(user.get('id')))
-    tasks = [{"task": _.get('title'), "completed": _.get('completed'),
-              "username": user.get('username')} for _ in data.json()]
-    output[user.get('id')] = tasks
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    output = {}
+    for user in users.json():
+        data = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(user.get('id')))
+        tasks = [{"task": _.get('title'), "completed": _.get('completed'),
+                "username": user.get('username')} for _ in data.json()]
+        output[user.get('id')] = tasks
 
-with open('todo_all_employees.json', 'w') as outfile:
-    json.dump(output, outfile)
+    with open('todo_all_employees.json', 'w') as outfile:
+        json.dump(output, outfile)
