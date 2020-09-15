4e89f379085afa37ad194da7d1d4e2a5c8ffa9b7
Nic Basilio <bnbasilio@gmail.com>
2020-09-15 06:27:34 +0000

    task 2 completed

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" 2. Export to JSON """
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(argv[1]))
+    jsonResponse = response.json()
+    USERNAME = jsonResponse["username"]
+
+    response = requests.get(
+               'https://jsonplaceholder.typicode.com/todos?userId={}'
+               .format(argv[1]))
+    jsonResponse = response.json()
+
+    data = {}
+    tasks = []
+    for item in jsonResponse:
+        tasks.append([{"task": item["title"],
+                       "completed": item["completed"],
+                       "username": USERNAME}])
+    data = {argv[1]: tasks}
+
+    with open("{}.json".format(argv[1]), 'w') as data_file:
+        json.dump(data, data_file)
