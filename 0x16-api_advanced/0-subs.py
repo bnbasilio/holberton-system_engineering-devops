25d8d75377ce1e9ebac79252309846c6733f56ec
Nic Basilio <bnbasilio@gmail.com>
2020-09-18 04:17:27 +0000

    task 0 completed

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 0. How many subs? """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    res = requests.get('https://reddit.com/r/{}/about.json'.format(subreddit),
+                       headers={'User-Agent': 'bnbasilio-app'})
+    try:
+        jsonResponse = res.json()
+        print (jsonResponse)
+        return jsonResponse["data"]["subscribers"]
+    except:
+        return 0
